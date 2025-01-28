195d4c51668a0b4d2b81b3cedffe085171f397a2
Anjalinegi <anjalinegi26500@gmail.com>
2025-01-29 01:36:11 +0530

    Update Swap.java

--- Swap.java
+++ Swap.java
@@ -1,17 +1,15 @@
-import java.util.Scanner;
-
+import java.util.Scanner;  
 public class Swap {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int a=sc.nextInt();
-        int b=sc.nextInt();
-        int c;
-        c=a;
-        a=b;
-        b=c;
-        System.out.println("a="+a);
-        System.out.println("b="+b);
-
+        Scanner sc = new Scanner(System.in);  
+        int a = sc.nextInt();  // Input first number
+        int b = sc.nextInt();  // Input second number
+        int temp;  
+        temp = a;  // Swap logic
+        a = b;  
+        b = temp;  
+        System.out.println("a=" + a);  // Display swapped value of a
+        System.out.println("b=" + b);  // Display swapped value of b
+        
     }
-    
 }
